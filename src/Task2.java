fb6f593141def8ebb06ece098ca7c88f8a202afc
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-06 14:00:01 +0400

    task1

---
+++ src/Task2.java
@@ -0,0 +1,11 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+
+    }
+     public static boolean task2(int [] nums){
+
+    }
+      return nums samefirst[0] == 0 && nums samelast [nums.length - 1] == 0);
+  }
+}
---
+++ src/commonEnd.java
@@ -0,0 +1,37 @@
+public class commonEnd {
+
+    public static void main(String[] args) {
+
+        System.out.println("boolean (int a, int b");
+        int a;
+        int b;
+        int samefirst;
+        int samelast;
+        int commonEn = ([1, 2, 3]), ([7, 3]));
+        int commonEn = ([1, 2, 3], [7, 3, 2])
+            int commonEnd= ([1, 2, 3], [1, 3]);
+        }
+      public static boolean
+        if (samefirst == 0 );// samelast == 0);
+        boolean = a ;
+        return true;
+
+    } else
+
+    {
+        boolean = b ;
+        return false;
+    }
+
+        if( [ samefirst.length - 1] == 0);) // samelast.length - 1] == 0);)
+
+
+    }
+
+
+
+
+
+
+    }
+}
