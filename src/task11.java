794138bd422b19c5736c1a6673896881802fbb07
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-07 08:39:33 +0400

    task11

---
+++ src/task11.java
@@ -0,0 +1,20 @@
+public class task11 {
+
+public static void main(String[] args){
+    public static Integer [] task11 (Integer[] int nums){
+
+        int maxEnd3 = [(1,2,3)];
+        int maxEnd3 = [(11,5,9)];
+        int maxEnd3 =[(2,11,3)];
+
+        int nums;
+        if (nums.length==3 || nums.length==3 {
+
+            nums = nums[0];
+        } else {
+            nums = nums[0];
+        }
+
+    }
+}
+}
